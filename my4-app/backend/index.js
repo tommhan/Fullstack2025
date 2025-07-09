021171edaabef4dcfb67e4ea5e2bbff78a324d66
hatom <tommhan@gmail.com>
2025-07-09 10:01:22 +0300

    lisätty generateId app.postin käyttöön

--- my4-app/backend/index.js
+++ my4-app/backend/index.js
@@ -23,12 +23,34 @@ let notes = [
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
 app.post('/api/notes', (request, response) => {
-  const note = request.body
-  console.log(note)
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
   response.json(note)
 })
-
+console.log("toimiiko post?")
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
