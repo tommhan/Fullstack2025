37dc9ed03a8ce12e6bbf45fdfee06a37dc8af916
hatom <tommhan@gmail.com>
2025-07-29 10:07:26 +0300

    render test

--- my4-app/backend/index.js
+++ my4-app/backend/index.js
@@ -30,10 +30,10 @@ const requestLogger = (request, response, next) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
-
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World! this is working?</h1>')
 })
 
 const generateId = () => {
